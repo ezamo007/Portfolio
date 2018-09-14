8797142e6834624ad7b0099298690dc18f6ffbe0
Eduardo Zamora <ezamo007@ucr.edu>
2018-09-13 21:30:34 -0500

    Added ucr faculty website redesign.

--- plot_fxy.py
+++ plot_fxy.py
@@ -4,9 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-domain = [-2*np.pi,2*np.pi,-2*np.pi,2*np.pi] #[x_min, x_max, y_min, y_max]
-#Finer resolution results in longer computation time but a smoother image.
-resolution = 0.5
+domain = [-10,10,-10,10] #[x_min, x_max, y_min, y_max]
+
+#Smaller resolution results in longer computation time but a smoother image.
+resolution = .01
 
 x_array = np.linspace(domain[0],domain[1], (domain[1]-domain[0])/resolution)
 y_array = np.linspace(domain[2],domain[3], (domain[3]-domain[2])/resolution)
@@ -15,12 +16,13 @@ y_array = np.linspace(domain[2],domain[3], (domain[3]-domain[2])/resolution)
 def f(coords):
     x = coords[0]
     y = coords[1]
-    return 50 * np.cos(x-2*y)
+    return 5*np.cos(x**2-x*y)
 
+#Red color for more negative values, Green for more positive values
 for x_point in x_array[:-1]:
     for y_point in y_array[:-1]:
         smushed_value = 1 / (1 + np.e**(-0.5 * f([x_point, y_point])))
         plt.arrow(x_point,y_point, 1,0, width= resolution, head_width = 0, color = (1-smushed_value, smushed_value,0) )
 
 plt.axis(domain)
-plt.show()
+plt.savefig("Portfolio\plot_fxy.png",  dpi = 600)
---
+++ ucr-math-faculty/js/script.js
@@ -0,0 +1,130 @@
+var ladder =  [
+              ['Alber, Mark', 'Distinguished Professor', 'Applied Mathematics, Mathematical and Computational Biology, Mathematical Physics', 'mark.alber@ucr.edu', 'http://mathdept.ucr.edu/images/Mark_Alber.jpg', 'http://mathdept.ucr.edu/faculty/alber.html'],
+              ['Baez, John', 'Professor', 'Mathematical Physics, N-Categories, Gravity', 'baez@math.ucr.edu', 'http://mathdept.ucr.edu/images/baez3.jpg', 'http://mathdept.ucr.edu/faculty/baez.html'],
+              ['Chang, Mei-Chu', 'Professor', 'Algebraic Geometry, Combinatorial Number Theory', 'mcc@math.ucr.edu', 'http://mathdept.ucr.edu/images/chang_web.png', 'http://mathdept.ucr.edu/faculty/chang.html'],
+              ['Chari, Vyjayanthi', 'Professor', 'Lie Theory', 'chari@math.ucr.edu', 'http://mathdept.ucr.edu/images/chariweb.jpg', 'http://mathdept.ucr.edu/faculty/chari.html'],
+              ['Chen, Po-Ning', 'Assistant Professor', 'Differential geometry and general relativity', 'poningc@ucr.edu', 'http://mathdept.ucr.edu/images/Poning_Chen.png', 'http://mathdept.ucr.edu/faculty/chen.html'],
+              ['Chen, Weitao', 'Assistant Professor', 'Numerical Method, Scientific Computing, Mathematical Biology', 'weitaoc@ucr.edu', 'http://mathdept.ucr.edu/images/wc2.jpg', 'http://mathdept.ucr.edu/faculty/weitaochen.html'],
+              ['Chow, Yat Tin', '', '', 'yattin.chow@ucr.edu', 'http://mathdept.ucr.edu/images/chow.jpg', 'http://mathdept.ucr.edu/faculty/chow.html'],
+              ['Costello, Kevin', 'Assistant Professor', 'Probabilistic Combinatorics', 'costello@math.ucr.edu', 'http://mathdept.ucr.edu/images/costello_web2.jpg', 'http://mathdept.ucr.edu/faculty/costello.html'],
+              ['Durham, Matthew', 'Assistant Professor', '', 'matthew.durham@ucr.edu', 'http://mathdept.ucr.edu/images/durham_news.png', 'http://mathdept.ucr.edu/faculty/durham.html'],
+              ['Gan, Wee Liang', 'Associate Professor / Department Vice Chair', 'Algebra, Representation Theory', 'wlgan@math.ucr.edu', 'http://mathdept.ucr.edu/images/ganweb.jpg', 'http://mathdept.ucr.edu/faculty/gan.html'],
+              ['Gonzalez, Jose', 'Assistant Professor', 'Algebraic Geometry', 'joselg@ucr.edu', 'http://mathdept.ucr.edu/images/JoseGonzalez.jpg', 'http://mathdept.ucr.edu/faculty/jgonzalez.html'],
+              ['Greenstein, Jacob', 'Professor', 'Lie Algebras, Quantum Affine Algebras, Representation Theory', 'green@math.ucr.edu', 'http://mathdept.ucr.edu/images/jacobgreenweb.jpg', 'http://mathdept.ucr.edu/faculty/greenstein.html'],
+              ['Guan, Zhuang-dan', 'Associate Professor', 'Complex Manifolds, Differential and Algebraic Geometry', 'zguan@math.ucr.edu', 'http://mathdept.ucr.edu/images/guanweb.jpg', 'http://mathdept.ucr.edu/faculty/guan.html'],
+              ['Kelliher, Jim', 'Associate Professor', 'Fluid Mechanics, Theoretical PDEs', 'kelliher@math.ucr.edu', 'http://mathdept.ucr.edu/images/kelliher_new.jpg', 'http://mathdept.ucr.edu/faculty/kelliher.html'],
+              ['Lapan, Sara', 'Assistant Professor of Teaching, Math (PSOE/SOE)', 'Holomorphic Dynamics, Several Complex Variables', 'slapan@math.ucr.edu', 'http://mathdept.ucr.edu/images/saralapan.jpg', 'http://mathdept.ucr.edu/faculty/lapan.html'],
+              ['Lapidus, Michel', 'Distinguished Professor', 'Mathematical Physics, Analysis/PDEs, Fractal Geometry, Spectral Geometry, Dynamical Systems', 'lapidus@math.ucr.edu', 'http://mathdept.ucr.edu/images/lapidus2web.gif', 'http://mathdept.ucr.edu/faculty/lapidus.html'],
+              ['Mautner, Carl', 'Assistant Professor', 'Representation Theory, Algebraic Geometry', 'mautner@math.ucr.edu', 'http://mathdept.ucr.edu/images/mautner_pp2.jpg', 'http://mathdept.ucr.edu/faculty/mautner.html'],
+              ['Moradifam, Amir', 'Assistant Professor', 'Partial Differential Equations, Inverse Problems, and Medical Imaging', 'moradifam@math.ucr.edu', 'http://mathdept.ucr.edu/images/moradifam_profile.jpg', 'http://mathdept.ucr.edu/faculty/moradifam.html'],
+              ['Poon, Yat Sun', 'Professor / Department Chair', 'Differential Geometry, Algebraic Geometry', 'ypoon@ucr.edu', 'http://mathdept.ucr.edu/images/poonweb10.jpg', 'http://mathdept.ucr.edu/faculty/poon.html'],
+              ['Ran, Ziv', 'Professor', 'Algebraic Geometry', 'ziv.ran@ucr.edu', 'http://mathdept.ucr.edu/images/ranweb2.jpg', 'http://mathdept.ucr.edu/faculty/ran.html'],
+              ['Rush, David', 'Professor', 'Commutative Algebra', 'rush@math.ucr.edu', 'http://mathdept.ucr.edu/images/rush2web.gif', 'http://mathdept.ucr.edu/faculty/rush.html'],
+              ['Samuelson, Peter', 'Assistant Professor', '', 'peter.samuelson@ucr.edu', 'http://mathdept.ucr.edu/images/samuelson.jpg', 'http://mathdept.ucr.edu/faculty/samuelson.html'],
+              ['Schultz, Reinhard', 'Professor', 'Topology, Transformation Groups', 'schultz@math.ucr.edu', 'http://mathdept.ucr.edu/images/schultz_web2.png', 'http://mathdept.ucr.edu/faculty/schultz.html'],
+              ['Vidussi, Stefano', 'Professor', 'Topology', 'svidussi@math.ucr.edu', 'http://mathdept.ucr.edu/images/vidussiweb.jpg', 'http://mathdept.ucr.edu/faculty/vidussi.html'],
+              ['Wang, Qixuan', 'Assistant Professor', '', 'qixuan.wang@ucr.edu', 'http://mathdept.ucr.edu/images/QixuanWang.jpg', 'http://mathdept.ucr.edu/faculty/qwang.html'],
+              ['Weisbart, David', 'Assistant Professor of Teaching, Math (PSOE/SOE)', 'Quantum Theory, Non-Archimedean Analysis, and Probability', 'weisbart@math.ucr.edu', 'http://mathdept.ucr.edu/images/wesibart_pp.jpg', 'http://mathdept.ucr.edu/faculty/weisbart.html'],
+              ['Wilhelm, Fred', 'Professor', 'Riemmanian Geometry', 'fred@math.ucr.edu', 'http://mathdept.ucr.edu/images/wilhelmweb2.jpg', 'http://mathdept.ucr.edu/faculty/wilhelm.html'],
+              ['Wong, Bun', 'Professor', 'Several Complex Variables, Complex Differential Geometry', 'wong@math.ucr.edu', 'http://mathdept.ucr.edu/images/wongweb.gif', 'http://mathdept.ucr.edu/faculty/wong.html'],
+              ['Xu, Feng', 'Professor', 'Operator Algebras', 'xufeng@math.ucr.edu', 'http://mathdept.ucr.edu/images/xu2web.gif', 'http://mathdept.ucr.edu/faculty/xu.html'],
+              ['Zhang, Qi', 'Professor/Grad Advisor Prospective Students', 'Partial Differential Equations, Geometric Analysis', 'qizhang@math.ucr.edu', 'http://mathdept.ucr.edu/images/zhangweb.jpg', 'http://mathdept.ucr.edu/faculty/zhang.html'],
+              ['Zhang, Zhenghe', 'Assistant Professor', 'Dynamical Systems, Spectral Theory, Mathematical Physics', 'zhenghe@ucr.edu', 'http://mathdept.ucr.edu/images/xyz.jpg', 'http://mathdept.ucr.edu/faculty/zzhang.html']
+              ];
+
+function create_profile(professor_info){
+  var name = professor_info[0],
+      position = professor_info[1],
+      research = professor_info[2],
+      email = professor_info[3],
+      image = professor_info[4],
+      link = professor_info[5];
+
+
+  var html = `
+            <div id = "profile", onmouseover = "profile_hover()">
+                <div id = "profile_top">
+                  <p class = "professor_name"> ` + name + `</p>
+                  <p class = "professor_details"> ` + position + `</p>
+                </div>
+
+                <div id = "profile_middle">
+                  <center>
+                  <a href = ` + link + `>
+                  <img src = ` + image + ` width = 100%>
+                  </a>
+                  </center>
+
+                </div>
+
+                <div id = "profile_bottom">
+                  <p class = "professor_details"> ` + research + `</p>
+                  </br>
+                  <p class = "professor_details", color = blue>`+ email + `</p>
+                </div>
+            </div>
+            `;
+  return html;
+}
+
+function arrange_cards(){
+  document.getElementById('faculty_left').innerHTML = ""
+  document.getElementById('faculty_middle').innerHTML = ""
+  document.getElementById('faculty_right').innerHTML = ""
+
+
+// document.elm.style.border = "3px solid #FF0000";
+
+  var i;
+
+  var pageWidth = document.documentElement.clientWidth;
+
+  if(pageWidth > 900){
+
+    document.getElementById("faculty_left").style.width= '33.3%';
+    document.getElementById("faculty_middle").style.width= '33.4%';
+    document.getElementById('faculty_right').style.width = '33.3%';
+    document.getElementById('faculty_right').visiblity = 'visible';
+
+
+      for(i=0; i< ladder.length; i++){
+          if(i%3 == 0){
+
+            document.getElementById('faculty_left').innerHTML += create_profile(ladder[i]);
+
+          }
+          if(i%3 == 1){
+            document.getElementById('faculty_middle').innerHTML += create_profile(ladder[i]);
+
+          }
+          if(i%3 == 2){
+            document.getElementById('faculty_right').innerHTML += create_profile(ladder[i]);
+        }
+      }
+    }
+
+    if(pageWidth <= 900){
+
+        document.getElementById("faculty_left").style.width= '50%';
+        document.getElementById("faculty_middle").style.width= '50%';
+        document.getElementById('faculty_right').visiblity = 'hidden';
+
+
+        for(i=0; i< ladder.length; i++){
+            if(i%2 == 0){
+              document.getElementById('faculty_left').innerHTML += create_profile(ladder[i]);
+            }
+
+            if(i%2 == 1){
+              document.getElementById('faculty_middle').innerHTML += create_profile(ladder[i]);
+
+            }
+        }
+
+      }
+    }
+
+function profile_hover(){
+}
+
+arrange_cards();
